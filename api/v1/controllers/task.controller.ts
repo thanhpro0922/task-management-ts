d266a9911ae3a343cf88b52adc64c5fe0ec424bf
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-17 19:36:16 +0700

    11. Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,9 +3,18 @@ import { Request, Response } from "express";
 import Task from "../model/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    interface Find {
+        deleted: boolean;
+        status?: string;
+    }
+    const find: Find = {
         deleted: false,
-    });
+    };
+
+    if (req.query.status) {
+        find.status = req.query.status.toString(); //! hoặc dùng như này thì bỏ interface, find["status"] = req.query.status;
+    }
+    const tasks = await Task.find(find);
     res.json(tasks);
 };
 
