ef4ea48c9ec26d92e82c9dc975bcbe14a7ee0bd8
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-19 00:18:51 +0700

    9. Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express"; //! import 2 thứ từ express là express và cái {dùng cái nào thì điền vào để lấy từ express}
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import * as database from "./config/database"; //! *as là trong cái file kia cs hàm gì thì import hết
 import mainV1Routes from "./api/v1/routes/index.route";
 
@@ -12,8 +13,16 @@ const app: Express = express(); //! : Express là kiểu Express do thg express 
 const port: number | string = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-mainV1Routes(app)
+// var corsOptions = {
+//     origin: "http://example.com", //! Nếu muốn định nghĩa cụ thể chỉ cho phép tên miền nào được sử dụng thôi thì bật cái comment này lên
+//     optionsSuccessStatus: 200,
+// };
+
+// app.use(cors());
+
+mainV1Routes(app);
 
 app.listen(port, () => {
     console.log(`Running port ${port} - By Diner`);
