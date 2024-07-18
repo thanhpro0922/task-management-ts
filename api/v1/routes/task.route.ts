99ed1c60e4c53e7d29a1b71882efa1994a7cb23d
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-18 23:50:00 +0700

    7. Xóa công việc

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -16,4 +16,6 @@ router.post("/create", controller.create);
 
 router.patch("/edit/:id", controller.edit);
 
+router.delete("/delete/:id", controller.deleteTask); //! Ở đây hơi khác tí là nếu mà ghi mỗi delete ko thì nó lỗi vì thằng typescript sợ trùng với cái hàm delete trong js nên là để khác delete là được
+
 export const taskRoutes: Router = router;
