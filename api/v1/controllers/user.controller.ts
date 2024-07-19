0f50c5776199dcb1f3fb2ea361b6dc02c579e700
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-19 21:18:41 +0700

    12. Tính năng đăng nhập

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -18,7 +18,7 @@ export const register = async (req: Request, res: Response) => {
     } else {
         req.body.password = md5(req.body.password);
 
-        req.body.token = generateRandomString(30)
+        req.body.token = generateRandomString(30);
 
         const user = new User(req.body);
         const data = await user.save();
@@ -31,4 +31,39 @@ export const register = async (req: Request, res: Response) => {
             token: token,
         });
     }
+};
+
+//% [POST] /api/v1/users/login
+export const login = async (req: Request, res: Response) => {
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false,
+    });
+
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email ko ton tai",
+        });
+        return;
+    }
+
+    if (md5(password) !== user.password) {
+        res.json({
+            code: 400,
+            message: "Sai pass",
+        });
+        return;
+    }
+
+    const token = user.token;
+
+    res.json({
+        code: 200,
+        message: "Đăng nhập thành công!",
+        token: token,
+    });
 };
--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -5,7 +5,7 @@ const router: Router = Router();
 
 router.post("/register", controller.register);
 
-// router.post("/login", controller.login);
+router.post("/login", controller.login);
 
 // router.post("/password/forgot", controller.forgotPassword);
 
